7b62b498c66e07ec5dc80cef9f172a18346ca56f
Sadowski-Krystian <80521867+Sadowski-Krystian@users.noreply.github.com>
2024-11-19 15:50:50 +0100

    Add files via upload

---
+++ monitor_module.c
@@ -0,0 +1,48 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+#include <linux/sched/loadavg.h>
+
+MODULE_AUTHOR("281443-279460");
+MODULE_DESCRIPTION("moduł do zasobów");
+MODULE_VERSION("1.0");
+
+static unsigned int interval = 10;
+module_param(interval, uint, 0644);
+MODULE_PARM_DESC(interval, "opóźnienie pomiędzy sprawdzeniami zuzycia procesora");
+
+static struct timer_list cpu_timer;
+
+
+static void log_cpu_usage(struct timer_list *t)
+{
+    unsigned long load = avenrun[0]; 
+    unsigned long load_percent = load * 100 / FIXED_1;
+
+    printk(KERN_INFO "CPU: %lu%%\n", load_percent);
+
+    mod_timer(&cpu_timer, jiffies + interval * HZ);
+}
+
+static int __init monitor_cpu_usage_init(void)
+{
+    printk(KERN_INFO "włączenie modułu. opóźnienie: %u sekund.\n", interval);
+
+    timer_setup(&cpu_timer, log_cpu_usage, 0);
+
+    mod_timer(&cpu_timer, jiffies + interval * HZ);
+
+    return 0;
+}
+
+static void __exit monitor_cpu_usage_exit(void)
+{
+    printk(KERN_INFO "wyłączenie modułu.\n");
+
+    del_timer(&cpu_timer);
+}
+
+module_init(monitor_cpu_usage_init);
+module_exit(monitor_cpu_usage_exit);
